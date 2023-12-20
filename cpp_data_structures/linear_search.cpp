fd6b14bad63f10ea4363b1f5be36806be3f4231c
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2023-12-20 15:11:56 +0500

    commit

--- cpp_data_structures/linear_search.cpp
+++ cpp_data_structures/linear_search.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+bool linear_search(int arr[], int n, int x)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+            return true;
+    }
+    return false;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "RESULT: ";
+    cout << linear_search(arr, n, 3) << endl;
     return 0;
 }
