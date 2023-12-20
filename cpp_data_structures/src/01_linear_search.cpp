aad72624bfd4313f83699492f928ddd4990b4780
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2023-12-20 15:53:20 +0500

    feat: implemented linear search in cpp

--- cpp_data_structures/src/01_linear_search.cpp
+++ cpp_data_structures/src/01_linear_search.cpp
@@ -1,9 +1,44 @@
+// Linear Search Algorithm Implemented In C++
+
 #include <iostream>
 
 using namespace std;
 
+bool linearSearch(int arr[], int value, int maxSize)
+{
+    for (int i = 0; i < maxSize; i++)
+    {
+        if (arr[i] == value)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main()
 {
-    cout << "Hello, World!" << endl;
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int maxSize = sizeof(arr) / sizeof(arr[0]);
+    int value = 5;
+
+    cout << "Array: ";
+    for (int i = i; i < maxSize; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Searching for value: " << value << endl;
+
+    if (linearSearch(arr, value, maxSize))
+    {
+        cout << "RESULT: Value FOUND" << endl;
+    }
+    else
+    {
+        cout << "RESULT: Value NOT FOUND" << endl;
+    }
+
     return 0;
 }
