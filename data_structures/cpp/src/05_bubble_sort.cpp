6f765d1e43ef50b91d1a4da843d763f9af0b149c
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2023-12-21 23:35:46 +0500

    minor changes

--- data_structures/cpp/src/05_bubble_sort.cpp
+++ data_structures/cpp/src/05_bubble_sort.cpp
@@ -1,1 +1,55 @@
 // Bubble Sort Algorithm
+
+#include <iostream>
+
+using namespace std;
+
+void bubble_sort(int array[], int max_size)
+{
+    while (true)
+    {
+        bool swapped = false;
+        for (int i = 0; i < max_size; i++)
+        {
+            if (array[i + 1] < array[i])
+            {
+                int temp = array[i + 1];
+                array[i + 1] = array[i];
+                array[i] = temp;
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+        {
+            break;
+        }
+    }
+}
+
+void display_array(int array[], int max_size)
+{
+    for (int i = 0; i < max_size; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int max_size = sizeof(array) / sizeof(array[0]);
+
+    // DIsplay Array Before Sorting
+    cout << "Array to sort: ";
+    display_array(array, max_size);
+
+    // Start Selection Sort
+    bubble_sort(array, max_size);
+
+    // Display Array After Sorting
+    cout << "Sorted array: ";
+    display_array(array, max_size);
+
+    return 0;
+}
