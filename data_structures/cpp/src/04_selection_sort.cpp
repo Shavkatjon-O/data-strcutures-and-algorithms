bfcec93579bf46f6b1d7efaffa2099c1217d3b5a
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2023-12-21 23:20:10 +0500

    minor changes

--- data_structures/cpp/src/04_selection_sort.cpp
+++ data_structures/cpp/src/04_selection_sort.cpp
@@ -4,15 +4,6 @@
 
 using namespace std;
 
-void display_array(int array[], int max_size)
-{
-    for (int i = 0; i < max_size; i++)
-    {
-        cout << array[i] << " ";
-    }
-    cout << endl;
-}
-
 void selection_sort(int array[], int max_size)
 {
     for (int i = 0; i < max_size; i++)
@@ -29,13 +20,32 @@ void selection_sort(int array[], int max_size)
         array[i] = array[min_index];
         array[min_index] = temp;
     }
-    display_array(array, max_size);
+}
+
+void display_array(int array[], int max_size)
+{
+    for (int i = 0; i < max_size; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
 }
 
 int main()
 {
     int array[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
     int max_size = sizeof(array) / sizeof(array[0]);
+
+    // DIsplay Array Before Sorting
+    cout << "Array to sort: ";
+    display_array(array, max_size);
+
+    // Start Selection Sort
     selection_sort(array, max_size);
+
+    // Display Array After Sorting
+    cout << "Sorted array: ";
+    display_array(array, max_size);
+
     return 0;
 }
